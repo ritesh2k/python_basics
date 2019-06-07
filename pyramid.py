1721a9e6ef9c48571dff7f3394d9de2b16976ed3
Ritesh Kumar <riiteshKumaar@gmail.com>
2019-06-07 16:09:20 +0530

    update pyramid.py to repo

---
+++ pyramid.py
@@ -0,0 +1,13 @@
+def pyramid(steps):
+	k=steps-1										#determining the number of spaces
+	for i in range(steps):							#contolling the outer loop
+
+		for j in range(k):
+			print (' ', end='')
+		k= k-1
+		for j in range (i+1):
+			print ('#', end=' ')
+		print()
+
+steps= int(input('Enter the number of steps\n'))
+pyramid(steps)	
