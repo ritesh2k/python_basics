37221021ece42eb620b840f03fa967e1657945db
Ritesh Kumar <riiteshKumaar@gmail.com>
2019-06-07 18:52:30 +0530

    added helix_pyramid

---
+++ helix_pyramid.py
@@ -0,0 +1,27 @@
+def rev_pyramid(steps):
+	k=0
+	for i in range(steps):
+		for j in range(k):
+			print(' ', end='')
+		k+=1
+		for j in range(steps-i):
+			print('#',end=' ')
+		print()
+
+
+
+def pyramid(n):
+	k=n-1
+	for i in range(n):
+
+		for j in range(k):
+			print (' ', end='')
+		k= k-1
+		for j in range (i+1):
+			print ('#', end=' ')
+		print()
+	
+
+steps=int(input('Enter the number of steps\n'))
+rev_pyramid(steps)
+rev_pyramid(steps)	
