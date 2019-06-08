864a9dcac7a0fdc97ffc514a5ab60c31c56e3dc0
Ritesh Kumar <riiteshKumaar@gmail.com>
2019-06-08 23:02:45 +0530

    updated README

--- max_char.py
+++ max_char.py
@@ -12,7 +12,7 @@ def max_char(str):
 		if max<dict[key]:
 			max=dict[key]
 			max_char=key
-	return max_char,max 		#returning a tuple results
+	return max_char,max 		#returning a tuple results		
 str=str(input('Enter the string to find the most frequent char\n')).replace(' ','') # removing the spaces
 print('The entered after removing spaces is :',str)
 print('The most frequent character is \'{}\' which appears {} times.'.format(max_char(str)[0],max_char(str)[1])) # using \ to insert ''
