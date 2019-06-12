3e38e01952d05cf133753d2889c8bacba2206bf5
Ritesh Kumar <riiteshKumaar@gmail.com>
2019-06-12 16:52:21 +0530

    added fib_pyramid.py

--- fib_pyramid.py
+++ fib_pyramid.py
@@ -6,12 +6,14 @@ def fibonacci(num):
 		return fibonacci(num-1)+fibonacci(num-2)
 
 def fib_pyramid(step):
-	print_num=0
-	for i in range(step):
-		for j in range(i+1):
+	print_num, spaces=0,0
+	for i in range(step):					#outer loop to control the steps
+		spaces=step-i-1						#controlling the spaces at the start of each steps
+		print (' '*spaces, end='')
+		for j in range(i+1):				#inner loop to call the fibonacci() function
 			print(fibonacci(print_num), end=' ')
 			print_num += 1
 		print()
 
-step=int(input('Enter the number of steps: '))
+step=int(input('Enter the number of steps: '))	#driver code
 fib_pyramid(step)		
